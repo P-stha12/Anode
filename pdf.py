40b9abb24c3478c2c92fe28b00f81fd225d9e1e9
Nitiz Khanal <67851367+StarAtNyte@users.noreply.github.com>
2022-12-10 14:08:17 +0545

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,8 +1,9 @@
 from fpdf import FPDF
 
-title = 'Title'
 
 class PDF(FPDF):
+
+
     def footer(self):
         # Position at 1.5 cm from bottom
         self.set_y(-15)
@@ -13,22 +14,14 @@ class PDF(FPDF):
         # Page number
         self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
 
-    def chapter_title(self, num, label):
-        # Arial 12
-        self.set_font('Arial', '', 12)
-        # Background color
-        self.set_fill_color(200, 220, 255)
-        # Title
-        self.cell(0, 6, 'Chapter %d : %s' % (num, label), 0, 1, 'L', 1)
-        # Line break
-        self.ln(4)
+    
 
     def chapter_body(self, name):
         # Read text file
         with open(name, 'rb') as fh:
             txt = fh.read().decode('latin-1')
         # Times 12
-        self.set_font('Times', '', 16)
+        self.set_font('Arial', '', 14)
         # Output justified text
         self.multi_cell(0, 5, txt)
         # Line break
@@ -39,8 +32,5 @@ class PDF(FPDF):
 
     def print_chapter(self, num, title, name):
         self.add_page()
-        self.chapter_title(num, title)
         self.chapter_body(name)
-
-pdf = PDF()
 
