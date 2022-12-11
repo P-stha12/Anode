783f550432ae3bedf7b40c42b1216a2f23b4ae04
Nitiz Khanal <67851367+StarAtNyte@users.noreply.github.com>
2022-12-11 23:18:59 +0545

    Update Sequel_And_prequel_Generator.py

--- pages/Sequel_And_prequel_Generator.py
+++ pages/Sequel_And_prequel_Generator.py
@@ -158,7 +158,7 @@ if st.button('Get Cover Image'):
                 
                 # Custom font style and font size
                 W = 768
-                title_font = ImageFont.truetype('playfair/playfair-font.ttf', 50)
+                title_font = ImageFont.truetype('playfair/playfair-font.ttf', 35)
                 author_font = ImageFont.truetype('playfair/playfair-font.ttf', 20)
                 title_text = f"{st.session_state.title}"
                 if ':' in title_text:
